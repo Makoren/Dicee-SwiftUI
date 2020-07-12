f425e3fa315505d715c5bfb602c96aed8f38b63a
Makoren <zodiac44@hotmail.com>
2020-07-12 10:45:28 +1000

    Initial Commit

---
+++ Dicee SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dicee SwiftUI
+//
+//  Created by Luke Lazzaro on 12/7/20.
+//  Copyright © 2020 Luke Lazzaro. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
